84ecd5fd0a1f16fa0477ca9ed30a75450ef37078
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-11 19:28:16 -0500

    Initial commit

---
+++ src/main/java/net/cocopup/enderores/EnderOresDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cocopup.enderores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnderOresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
